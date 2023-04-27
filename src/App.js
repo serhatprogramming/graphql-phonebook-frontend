c1b62651cabdb09f355a4e6d455326085f81c9af
serhatprogramming <serhatprogramming@gmail.com>
2023-04-27 12:13:14 -0400

    useQuery query creation and running the query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+// apollo client useQuery
+import { gql, useQuery } from "@apollo/client";
+
+// create a query for allpersons
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+
 const App = () => {
-  return <div>Hello...</div>;
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>{result.data.allPersons.map((person) => person.name).join(", ")}</div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // apollo client and graphql
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
 
 // setup client
 const client = new ApolloClient({
@@ -32,4 +37,8 @@ client.query({ query }).then((response) => {
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
