0b8e4c7bf203884e5f04119d56fe4b953ed164f4
serhatprogramming <serhatprogramming@gmail.com>
2023-04-27 11:55:09 -0400

    set client query and send a query to the server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// apollo client and graphql
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+// setup client
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+// create gql queries
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        city
+        street
+      }
+      id
+    }
+  }
+`;
+
+// send a query to the server
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
